e78f4859e528098f23ffe12e2233a31faefe932f
Stephen Tillman <askmrtillman@gmail.com>
2021-05-22 01:51:02 -0400

    handle incoming twitter icons

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// TODO: clean up / refactor
 const { GravatarUpdater } = require('./gravatar-updater');
+const { AvbxTwitterClient } = require('avatarbox.sdk');
+const { TwitterService } = require('./services/twitter.service');
 
 const handler = (event) => {
   
@@ -6,14 +9,23 @@ const handler = (event) => {
     console.info('ping!');
     return;
   }
-
+  
   Promise.all(
-    event.Records.map(record => {
+    event.Records.map(async record => {
       if(!record.body) return;
-      const { id, source } = record.body;
-      // TODO: handle when source = twitter/gravatar
-      const gravatarUpdater = new GravatarUpdater();
-      return gravatarUpdater.update(record.body.toString());
+      const [ id, source ] = record.body.split(',');
+      if(/gravatar/.test(source)){
+        const gravatarUpdater = new GravatarUpdater();
+        return await gravatarUpdater.update(id);
+      } else {
+        const client = new AvbxTwitterClient();
+        const profile = await client.fetch(id.toString());
+        const index = (profile.currentAvatarIndex + 1) % profile.avatars.length;
+        const twitterService = new TwitterService(profile.token, profile.tokenSecret);
+        const imageUrl = profile.avatars[index];
+        await twitterService.updateProfileIcon(imageUrl);
+        await client.reset({ id, imageUrl });
+      }
     })
   ).catch(err => {
     console.error('update failed: ', err);
